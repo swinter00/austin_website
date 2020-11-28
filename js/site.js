4ad317cd177c14e53caae83668de29ff91010448
swinter00 <58004944+swinter00@users.noreply.github.com>
2020-11-28 14:30:33 -0500

    Mobile toggle nav created, implemented on gigs page

---
+++ js/site.js
@@ -0,0 +1,19 @@
+function dropdownToggle() {
+    if (document.querySelector(".drop_list").classList.contains("show")) {
+        document.querySelector(".drop_list").classList.remove("show");
+    } else {
+        document.querySelector(".drop_list").classList.add("show");
+    }
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.drop_button')) {
+      var items = document.getElementsByClassName("drop_list");
+      for (var i = 0; i < items.length; i++) {
+        var open = items[i];
+        if (open.classList.contains('show')) {
+          open.classList.remove('show');
+        }
+      }
+    }
+  } 
