d939e3f1283da3ef89a9398d4e0915e95d9890b7
swinter00 <58004944+swinter00@users.noreply.github.com>
2020-12-02 15:14:27 -0500

    Finished desktop view, mobile about images limited

--- js/site.js
+++ js/site.js
@@ -1,19 +1,19 @@
 function dropdownToggle() {
-    if (document.querySelector(".drop_list").classList.contains("show")) {
-        document.querySelector(".drop_list").classList.remove("show");
-    } else {
-        document.querySelector(".drop_list").classList.add("show");
-    }
+  if (document.querySelector(".drop_list").classList.contains("show")) {
+      document.querySelector(".drop_list").classList.remove("show");
+  } else {
+      document.querySelector(".drop_list").classList.add("show");
   }
+}
   
   window.onclick = function(event) {
-    if (!event.target.matches('.drop_button')) {
-      var items = document.getElementsByClassName("drop_list");
-      for (var i = 0; i < items.length; i++) {
-        var open = items[i];
-        if (open.classList.contains('show')) {
-          open.classList.remove('show');
-        }
+  if (!event.target.matches('.drop_button')) {
+    var items = document.getElementsByClassName("drop_list");
+    for (var i = 0; i < items.length; i++) {
+      var open = items[i];
+      if (open.classList.contains('show')) {
+        open.classList.remove('show');
       }
     }
-  } 
+  }
+}
